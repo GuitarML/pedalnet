fe6d26ac707a275db4e27b0efe21fe697bb17f58
Keith <kbloemer@aegistg.com>
2020-08-06 14:39:06 -0500

    Fixed indexing error

--- convert_pedalnet_to_wavenetva.py
+++ convert_pedalnet_to_wavenetva.py
@@ -25,7 +25,6 @@ def convert(args):
     # Permute tensors to match Tensorflow format with .permute(a,b,c):
     #a, b, c = 0, 1, 2  # Original shape  (creates heavily distorted signal for ts9, but not right)
     a, b, c = 2, 1, 0  # Pytorch uses (out_channels, in_channels, kernel_size), TensorFlow uses (kernel_size, in_channels, out_channels)
-
     model = PedalNet.load_from_checkpoint(checkpoint_path=args.model)
 
     sd = model.state_dict()
@@ -49,35 +48,35 @@ def convert(args):
         # Input Layer
         if i == -1: 
             data_out["variables"].append({"layer_idx":i,
-                                        "data":[str(i) for i in (sd['wavenet.input_layer.weight']).permute(a,b,c).flatten().numpy().tolist()],
+                                        "data":[str(w) for w in (sd['wavenet.input_layer.weight']).permute(a,b,c).flatten().numpy().tolist()],
                                         "name":"W"})
             data_out["variables"].append({"layer_idx":i,
-                                        "data":[str(i) for i in (sd['wavenet.input_layer.bias']).flatten().numpy().tolist()],
+                                        "data":[str(b) for b in (sd['wavenet.input_layer.bias']).flatten().numpy().tolist()],
                                         "name":"b"})
         # Linear Mix Layer
         elif  i == len(dilations):  
             data_out["variables"].append({"layer_idx":i,
-                                        "data":[str(i) for i in (sd['wavenet.linear_mix.weight']).permute(a,b,c).flatten().numpy().tolist()], 
+                                        "data":[str(w) for w in (sd['wavenet.linear_mix.weight']).permute(a,b,c).flatten().numpy().tolist()], 
                                         "name":"W"})
 
             data_out["variables"].append({"layer_idx":i,
-                                        "data":[str(i) for i in (sd['wavenet.linear_mix.bias']).numpy().tolist()],
+                                        "data":[str(b) for b in (sd['wavenet.linear_mix.bias']).numpy().tolist()],
                                         "name":"b"})
         # Hidden Layers
         else:
             data_out["variables"].append({"layer_idx":i,
-                                    "data":[str(i) for i in sd['wavenet.convs_tanh.' + str(i) + '.weight'].permute(a,b,c).flatten().numpy().tolist() +
+                                    "data":[str(w) for w in sd['wavenet.convs_tanh.' + str(i) + '.weight'].permute(a,b,c).flatten().numpy().tolist() +
                                     sd['wavenet.convs_sigm.' + str(i) + '.weight'].permute(a,b,c).flatten().numpy().tolist()], 
                                     "name":"W_conv"})
             data_out["variables"].append({"layer_idx":i,
-                                        "data":[str(i) for i in sd['wavenet.convs_tanh.' + str(i) + '.bias'].flatten().numpy().tolist() + 
+                                        "data":[str(b) for b in sd['wavenet.convs_tanh.' + str(i) + '.bias'].flatten().numpy().tolist() + 
                                         sd['wavenet.convs_sigm.' + str(i) + '.bias'].flatten().numpy().tolist()],
                                         "name":"b_conv"})
             data_out["variables"].append({"layer_idx":i,
-                                        "data":[str(i) for i in sd['wavenet.residuals.' + str(i) + '.weight'].permute(a,b,c).flatten().numpy().tolist()],
+                                        "data":[str(w2) for w2 in sd['wavenet.residuals.' + str(i) + '.weight'].permute(a,b,c).flatten().numpy().tolist()],
                                         "name":"W_out"})
             data_out["variables"].append({"layer_idx":i,
-                                        "data":[str(i) for i in sd['wavenet.residuals.' + str(i) + '.bias'].flatten().numpy().tolist()],
+                                        "data":[str(b2) for b2 in sd['wavenet.residuals.' + str(i) + '.bias'].flatten().numpy().tolist()],
                                         "name":"b_out"})
 
     #for debugging ###################
