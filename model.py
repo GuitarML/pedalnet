e57e5639101a6f64976df0b91867cfcec934be43
keith <kbloemer89@gmail.com>
2020-08-28 19:02:41 -0500

    Updated model to handle different number of channels

--- model.py
+++ model.py
@@ -72,7 +72,7 @@ class WaveNet(nn.Module):
             out_channels=1,
             kernel_size=1,
         )
-
+        self.num_channels = num_channels
 
     def forward(self, x):
         out = x
@@ -87,7 +87,7 @@ class WaveNet(nn.Module):
 
             # gated activation
             #   split (32,16,3) into two (16,16,3) for tanh and sigm calculations
-            out_hidden_split = torch.split(out_hidden, 16, dim=1)  
+            out_hidden_split = torch.split(out_hidden, self.num_channels, dim=1)  
             out = torch.tanh(out_hidden_split[0]) * torch.sigmoid(out_hidden_split[1])
 
             skips.append(out)
